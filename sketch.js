fc63cdf9020e0430092a4c22b6ade389955bac13
MaroMan0007 <43013047+MaroMan0007@users.noreply.github.com>
2018-12-03 15:19:44 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var person;
+
+
+//var obstacle;
+
+
+function setup() {
+  createCanvas(640, 360);
+  person = new Person();
+}
+
+
+
+
+function keyPressed() {
+  if (key == ' ') {
+    
+    var jump = createVector(0, -1);
+    person.applyForce(jump);
+  }
+}
+
+
+
+function draw() {
+  background(51);
+  
+  var gravity = createVector(0, 0.1);
+  person.applyForce(gravity);
+  
+  translate(-person.pos.x+50, 0);
+  
+  
+  if(mouseIsPressed) {
+  var force = createVector(-0.01, 0);
+  person.applyForce(force);
+  }
+  
+  person.update();
+  person.edges();
+  person.display();
+  
+  
+  fill(255, 0, 100);
+  rect(400, height-50, 50, 50);
+  
+  }
